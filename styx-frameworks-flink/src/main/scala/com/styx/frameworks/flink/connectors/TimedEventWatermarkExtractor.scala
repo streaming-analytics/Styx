5bc4946e245e714a4ccc220f97df5e267271d6b4
Bas Geerdink <bas@gdink.com>
2018-04-26 11:05:32 +0200

    final teqnation presentation

--- styx-frameworks-flink/src/main/scala/com/styx/frameworks/flink/connectors/TimedEventWatermarkExtractor.scala
+++ styx-frameworks-flink/src/main/scala/com/styx/frameworks/flink/connectors/TimedEventWatermarkExtractor.scala
@@ -2,7 +2,7 @@ package com.styx.frameworks.flink.connectors
 
 import com.styx.domain.events.TimedEvent
 import com.styx.common.Logging
-import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks
+import org.apache.flink.streaming.api.functions.{AssignerWithPeriodicWatermarks, AssignerWithPunctuatedWatermarks}
 import org.apache.flink.streaming.api.watermark.Watermark
 
 import scala.reflect.ClassTag
@@ -14,6 +14,17 @@ class TimedEventWatermarkExtractor[T <: TimedEvent : ClassTag]() extends Assigne
   }
 
   @Override def extractTimestamp(element: T, previousElementTimestamp: Long): Long = {
+    element.eventTime.getMillis   // no delay
+  }
+}
+
+class PeriodicTimedEventWatermarkExtractor[T <: TimedEvent : ClassTag]() extends AssignerWithPeriodicWatermarks[T]() with Logging {
+
+  override def extractTimestamp(element: T, previousElementTimestamp: Long): Long = {
     element.eventTime.getMillis
+  }
+
+  override def getCurrentWatermark: Watermark = {
+    new Watermark(System.currentTimeMillis() - 1000)   // 1 second delay for processing of window
   }
 }
