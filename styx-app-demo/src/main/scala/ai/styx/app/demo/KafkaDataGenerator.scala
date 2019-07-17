18109e5ea503680f8df26aa6c921a52b83faaaec
Bas Geerdink <bas@gdink.com>
2019-07-16 18:51:51 -0700

    updated oscon presentation

--- styx-app-demo/src/main/scala/ai/styx/app/demo/KafkaDataGenerator.scala
+++ styx-app-demo/src/main/scala/ai/styx/app/demo/KafkaDataGenerator.scala
@@ -17,4 +17,15 @@ object KafkaDataGenerator extends App {
   producer.send("test 1234")
 
   producer.close(1000L, TimeUnit.MILLISECONDS)
+
+
+  // loop
+
+  // generate x tweets per second
+
+  // select a random tweet from the tweets file
+
+  // adjust the date (event time)
+
+  // publish the tweet
 }
