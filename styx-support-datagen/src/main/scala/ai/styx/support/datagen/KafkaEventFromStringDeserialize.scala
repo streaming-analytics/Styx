9cbb0f9c74763871f7c5445fda96e5f727bc47e5
Bas Geerdink <bas@gdink.com>
2020-05-10 12:14:27 +0200

    removed old demo producers

--- styx-support-datagen/src/main/scala/ai/styx/support/datagen/KafkaEventFromStringDeserialize.scala
+++
@@ -1,8 +0,0 @@
-package ai.styx.support.datagen
-
-import ai.styx.domain.events.BaseKafkaEvent
-import org.apache.flink.api.common.functions.RichMapFunction
-
-class KafkaEventFromStringDeserialize(topicName: String) extends RichMapFunction[String, BaseKafkaEvent] {
-  def map(in: String): BaseKafkaEvent = BaseKafkaEvent(topicName, RawEventGenerator.parseRawEvent(in))
-}
